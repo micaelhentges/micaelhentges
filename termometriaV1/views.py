f5eb70fe3c59af44d690d2e4976e2ebb58f78407
Micael Thales Hentges <micael.hentges@kepler.com.br>
2022-08-25 15:03:49 -0300

    ajsutes

--- termometriaV1/views.py
+++ termometriaV1/views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
 from termometriaV1.models import SiloV1
 from termometriaV1.serializer import SiloV1Serializer
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+#from rest_framework.authentication import BasicAuthentication
+#from rest_framework.permissions import IsAuthenticated
 
 class SilosV1ViewSet(viewsets.ModelViewSet):
     """Exibindo todos os silos"""
     queryset = SiloV1.objects.all()
     serializer_class = SiloV1Serializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authentication_classes = [BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
